d484e7e2246fd1cbbd6e26c189463f0cdc44d6c9
Mr.Blinky <4971163+MrBlinky@users.noreply.github.com>
2019-07-16 01:00:51 +0200

    update
    Added drag button support
    optimized  |= into +=
    added round cornered tilesheets

--- walldemo/walldemo.ino
+++ walldemo/walldemo.ino
@@ -19,6 +19,7 @@
 Arduboy2Base arduboy;
 uint8_t borderTile; 
 Point cursor;
+bool cursor_changed;
 uint8_t cursor_frame;
 
 void setup()
@@ -39,19 +40,19 @@ void  drawWalls()
     {
      //// draw outlined wall tiles ////
      if (getTile(x,y) == WALL)
-     {
+     { asm volatile("dbg:\n");
        uint8_t wall=0;
-       if (getTile(x + 0, y - 8) == WALL) wall |= 0x01;
-       if (getTile(x - 8, y + 0) == WALL) wall |= 0x02;
-       if (getTile(x + 0, y + 8) == WALL) wall |= 0x04;
-       if (getTile(x - 8, y - 8) == WALL) wall |= 0x08;
+       if (getTile(x + 0, y - 8) == WALL) wall += 0x01;
+       if (getTile(x - 8, y + 0) == WALL) wall += 0x02;
+       if (getTile(x + 0, y + 8) == WALL) wall += 0x04;
+       if (getTile(x - 8, y - 8) == WALL) wall += 0x08;
        uint8_t tile = pgm_read_byte(&wallDecodeTable[wall]);
        if (getTile(x - 8, y + 8) == WALL) tile = tile / 16;
        tile &= 0x0F;
        Sprites::drawSelfMasked(x, y, leftwalltiles, tile);
        wall &= 0x05; //clear left walls
-       if (getTile(x + 8, y + 0) == WALL) wall |= 0x02;
-       if (getTile(x + 8, y - 8) == WALL) wall |= 0x08;
+       if (getTile(x + 8, y + 0) == WALL) wall += 0x02;
+       if (getTile(x + 8, y - 8) == WALL) wall += 0x08;
        tile = pgm_read_byte(&wallDecodeTable[wall]);
        if (getTile(x + 8, y + 8) == WALL) tile = tile / 16;
        tile &= 0x0F;
@@ -65,16 +66,17 @@ void loop()
   if (!arduboy.nextFrame()) return; //wait for start of frame
   if (arduboy.everyXFrames(7)) cursor_frame = (cursor_frame + 1) & 3;// 4 frames
   arduboy.pollButtons();
-  if (arduboy.justPressed(UP_BUTTON)) cursor.y--;
-  if (arduboy.justPressed(DOWN_BUTTON)) cursor.y++;
-  if (arduboy.justPressed(LEFT_BUTTON)) cursor.x--;
-  if (arduboy.justPressed(RIGHT_BUTTON)) cursor.x++;
+  cursor_changed = false;  
+  if (arduboy.justPressed(UP_BUTTON)) {cursor.y--; cursor_changed = true;}
+  if (arduboy.justPressed(DOWN_BUTTON)) {cursor.y++; cursor_changed = true;}
+  if (arduboy.justPressed(LEFT_BUTTON)) {cursor.x--; cursor_changed = true;}
+  if (arduboy.justPressed(RIGHT_BUTTON)) {cursor.x++; cursor_changed = true;}
   cursor.y &= 0x07; //keep y in range 0..7
   cursor.x &= 0x0F; //keep x in range 0..15
-  if (arduboy.justPressed(A_BUTTON)) mapBuffer[cursor.y][cursor.x] ^= WALL;
+  if (arduboy.justPressed(A_BUTTON) || (arduboy.pressed(A_BUTTON) && cursor_changed)) mapBuffer[cursor.y][cursor.x] ^= WALL;
   if (arduboy.justPressed(B_BUTTON)) borderTile ^= WALL;
   drawWalls();
   Sprites::drawPlusMask(cursor.x * 8, cursor.y * 8, cursorsprite, cursor_frame);
-  arduboy.display(CLEAR_BUFFER);  
+  arduboy.display(CLEAR_BUFFER);
 }
 
